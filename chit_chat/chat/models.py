6cca35b45273185159447aabb724ccc857d37b05
KianMehrabi <kian.mehrabi123@gmail.com>
2026-08-30 23:54:36 +0330

    i think i made the models work

--- chit_chat/chat/models.py
+++ chit_chat/chat/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserRoomCard(models.Model):
+    joined_at = models.TimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User , on_delete=models.CASCADE)
+    is_muted = models.BooleanField(default=False)
+
+class Room(models.Model):
+    user = models.ForeignKey(UserRoomCard ,on_delete=models.CASCADE)
+
+
+
+class Message(models.Model):
+    created_at = models.TimeField(auto_now=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE , null=True)
+    created_by = models.ForeignKey(User ,on_delete=models.CASCADE)
+    content = models.TextField(max_length = 150)
+    is_seen = models.BooleanField(default=False)
+    is_sent = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Message"
+        ordering = ["created_at"]
